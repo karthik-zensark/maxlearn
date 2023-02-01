44ec75ad4a24246f915b0c156281c8e55b56bcdc
karthik-zensark <karthik.gaddam@zensark.com>
2023-02-02 04:47:51 +0530

    Complete reformat of API response and minor visual changes

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -89,6 +89,10 @@ export class PageComponent implements AfterViewInit {
     total_tokens: 0,
   };
 
+  finalQuestionsArr: any[] = [];
+  finalFlashcardsArr: any[] = [];
+  finalKeypointsArr: any[] = [];
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {}
@@ -262,21 +266,57 @@ export class PageComponent implements AfterViewInit {
       default:
         finalString = "Multiple Choice Questions";
     }
-    if (parseInt(numberOfQuestions) > 1) {
+    if (
+      parseInt(numberOfQuestions) > 1 &&
+      finalString == "Multiple Choice Question"
+    ) {
       finalString =
         numberOfQuestions +
         " " +
         finalString +
         "s" +
         " " +
-        "where Question must be Q followed by Question number";
-    } else if (parseInt(numberOfQuestions) == 1) {
+        "With atleast 4 options Must state each question as 'Question:', option as 'Option:', answer as 'Answer:' and feedback as 'Feedback:'";
+    } else if (
+      parseInt(numberOfQuestions) == 1 &&
+      finalString == "Multiple Choice Question"
+    ) {
       finalString =
         numberOfQuestions +
         " " +
         finalString +
         " " +
-        "where Question must be Q followed by Question number";
+        "With atleast 4 options Must state question as 'Question:', option as 'Option:', answer as 'Answer:' and feedback as 'Feedback:'";
+    } else if (parseInt(numberOfQuestions) > 1 && finalString == "Flashcard") {
+      finalString =
+        numberOfQuestions +
+        " " +
+        finalString +
+        "s" +
+        " " +
+        "Stating question as 'Question:' and answer as 'Answer:'";
+    } else if (parseInt(numberOfQuestions) == 1 && finalString == "Flashcard") {
+      finalString =
+        numberOfQuestions +
+        " " +
+        finalString +
+        " " +
+        "Stating question as 'Question:' and answer as 'Answer:'";
+    } else if (parseInt(numberOfQuestions) > 1) {
+      finalString =
+        numberOfQuestions +
+        " " +
+        finalString +
+        "s" +
+        " " +
+        "Stating every point as 'Keypoint:'";
+    } else {
+      finalString =
+        numberOfQuestions +
+        " " +
+        finalString +
+        " " +
+        "Stating every point as 'Keypoint:'";
     }
     return finalString;
   }
@@ -286,7 +326,8 @@ export class PageComponent implements AfterViewInit {
     // let highlightTag =
     // "Highlight the correct answer and provide feedback. and structure the response in a html document";
     // ** without formatting the response in HTML
-    let highlightTag = "Highlight the correct answer and provide feedback.";
+    let highlightTag =
+      "Must Highlight the correct answer and provide feedback.";
     if (this.selectedCategory.name === this.category[0].name) {
       return highlightTag;
     } else {
@@ -384,37 +425,228 @@ export class PageComponent implements AfterViewInit {
 
   setGeneratedQuestion(recievedQuestion: string) {
     console.log("setGeneratedQuestion started..");
-    this.finalReplacedAnswer = this.finalAnswer.replace(
-      /\n{2}/g,
-      "</br></br></br>"
-    );
-    this.finalReplacedAnswer = this.finalReplacedAnswer.replace(
-      /\n/g,
-      "</br></br>"
-    );
-    // let tempArr = this.finalAnswer.split("\n");
-    // let tempArr0 = tempArr[2].split(" ");
-    // let questionsArr: any[] = [];
-    // tempArr.forEach((ele, eleIndex) => {
-    //   let isQuestion = ele.search(/^Q/);
-    //   let allArr: any[] = [];
-    //   console.log(isQuestion);
-    //   if (isQuestion == 0) {
-    //     if (allArr.length > 0) {
-    //       questionsArr.push(allArr);
-    //       allArr = [];
-    //     }
-    //     allArr.push(ele);
-    //   } else if (isQuestion == -1) {
-    //     allArr.push(ele);
-    //   }
-    // });
-    // console.log(questionsArr);
-    // this.scrollToBottom();
-    // console.log(tempArr);
-    // console.log(tempArr0);
+    // ** Code for replacing /n with </br>
+    // this.finalReplacedAnswer = this.finalAnswer.replace(
+    //   /\n{2}/g,
+    //   "<b></br></br></br>"
+    // );
+    // this.finalReplacedAnswer = this.finalReplacedAnswer.replace(
+    //   /\n/g,
+    //   "</br></br>"
+    // );
+    // ** Code for Splitting the response into several required arrays instead of adding <br>
+    let tempArr = this.finalAnswer.split("\n");
+    let questionsArr: any[] = [];
+    let flashcardsArr: any[] = [];
+    let keyPointsArr: any[] = [];
+    let tempQuestionArr: any[] = [];
+    let tempFlashcardsArr: any[] = [];
+    let tempKeypointsArr: any[] = [];
+    tempArr.forEach((ele, eleIndex) => {
+      if (this.selectedCategory.name == "Questions") {
+        let isQuestion = ele.search(/^(Q|q)uestion|^(Q|q)/);
+        let isOption = ele.search(
+          /^(O|o)ption|^.[a-d]|^[a-d]|^[a-d].|^.[A-D]|^[A-D]|^[A-D].|^.[1-4]|^[1-4]|^[1-4]./
+        );
+        let isAnswer = ele.search(/^(A|a)nswer/);
+        let isFeedback = ele.search(/^(F|f)eedback/);
+        if (isQuestion == 0) {
+          let question = { question: ele };
+          tempQuestionArr.push(question);
+        } else if (isAnswer == 0) {
+          let answer = { answer: ele };
+          tempQuestionArr.push(answer);
+        } else if (isOption == 0) {
+          let optionArr: any[] = [];
+          let options = { options: optionArr };
+          options.options.push(ele);
+          tempQuestionArr.push(options);
+        } else if (isFeedback == 0) {
+          let feedback = { feedback: ele };
+          tempQuestionArr.push(feedback);
+          questionsArr.push(tempQuestionArr);
+          tempQuestionArr = [];
+        }
+      } else if (this.selectedCategory.name == "Flashcards") {
+        let isQuestion = ele.search(/^(Q|q)uestion|^(Q|q)/);
+        let isAnswer = ele.search(/^(A|a)nswer/);
+        if (isQuestion == 0) {
+          let question = { question: ele };
+          tempFlashcardsArr.push(question);
+        } else if (isAnswer == 0) {
+          let answer = { answer: ele };
+          tempFlashcardsArr.push(answer);
+          flashcardsArr.push(tempFlashcardsArr);
+          tempFlashcardsArr = [];
+        }
+      } else if (this.selectedCategory.name == "Key Learning Points (KLPs)") {
+        let isKey = ele.search(/^(K|k)ey/);
+        if (isKey == 0) {
+          let keyPoint = { keyPoint: ele };
+          tempKeypointsArr.push(keyPoint);
+          keyPointsArr.push(tempKeypointsArr);
+          tempKeypointsArr = [];
+        }
+      }
+    });
+    console.log("Question Array ----------> ", questionsArr);
+    this.questionArraySetter(questionsArr);
+    console.log("Flashcards Array ----------> ", flashcardsArr);
+    this.flashCardsArraySetter(flashcardsArr);
+    console.log("Key Points Array ----------> ", keyPointsArr);
+    this.keyPointsArraySetter(keyPointsArr);
+    this.scrollToBottom();
+    console.log(tempArr);
     console.log(this.finalReplacedAnswer);
     this.scrollToBottom();
+  }
+
+  questionArraySetter(questionsArray: any[]) {
+    let tempQuestionsArr: any[] = [];
+    questionsArray.forEach((questionArray, questionArrayIndex) => {
+      let tempQuestionArray: any[] = [];
+      let optionsArr: any[] = [];
+      let options = { options: optionsArr };
+      questionArray.forEach((ele: any, eleIndex: number) => {
+        console.log(Object.keys(ele)[0]);
+        let question = { question: "" };
+        let answer = { answer: "" };
+        let feedback = { feedback: "" };
+        let keyName: string = Object.keys(ele)[0];
+        if (keyName == "question" || keyName == "Question") {
+          console.log(ele.question);
+          let replacedQuestion: string = ele.question.replace(
+            /^(Q|q)uestion:|^(Q|q)uestion/,
+            ""
+          );
+          question.question = replacedQuestion;
+          tempQuestionArray.push(question);
+        }
+        if (
+          keyName == "option" ||
+          keyName == "Option" ||
+          keyName == "options" ||
+          keyName == "Options"
+        ) {
+          console.log(ele);
+          // let isOption = ele.options[0].search(
+          //   /^Option|^option|^[a-d]|^[A-D]|^[1-4]/
+          // );
+          // if (isOption == 0) {
+          let replacedOption: string = ele.options[0].replace(
+            /^(O|o)ption:|^(O|o)ption|^(O|o)ption\s([A-D]|[a-d]|[1-4]):|^(O|o)ption\s([A-D]|[a-d]|[1-4])/,
+            ""
+          );
+          options.options.push(replacedOption);
+          // }
+        }
+        if (keyName == "answer" || keyName == "Answer") {
+          let replacedAnswer: string = ele.answer.replace(
+            /^(A|a)nswer:|^(A|a)nswer/,
+            ""
+          );
+          answer.answer = replacedAnswer;
+          tempQuestionArray.push(options);
+          tempQuestionArray.push(answer);
+        }
+        if (keyName == "feedback" || keyName == "Feedback") {
+          // let replacedKeypoint: string = ele.feedback.replace(
+          //   /^(K|k)eypoint:|^(K|k)eypoint|^(K|k)ey\spoint|^(K|k)ey\sPoint/,
+          //   ""
+          // );
+          let replacedFeedback: string = ele.feedback.replace(
+            /^(F|f)eedback:|^(F|f)eedback/,
+            ""
+          );
+          feedback.feedback = replacedFeedback;
+          tempQuestionArray.push(feedback);
+        }
+      });
+      tempQuestionsArr.push(tempQuestionArray);
+    });
+    this.finalQuestionsArr = tempQuestionsArr;
+    console.log(
+      "FInal question array arr: ----------> ",
+      this.finalQuestionsArr
+    );
+  }
+
+  flashCardsArraySetter(flashCardsArray: any[]) {
+    let tempFlashCardsArr: any[] = [];
+    flashCardsArray.forEach((flashCard, flashCardIndex) => {
+      let tempFlashCardArray: any[] = [];
+      flashCard.forEach((ele: any, eleIndex: any) => {
+        let question = { question: "" };
+        let answer = { answer: "" };
+        let keyName: string = Object.keys(ele)[0];
+        console.log(keyName);
+        if (
+          keyName === "question" ||
+          keyName === "Question" ||
+          keyName === "questions" ||
+          keyName === "Questions"
+        ) {
+          console.log(ele.question);
+          let replacedQuestion: string = ele.question.replace(
+            /^(Q|q)uestion:|^(Q|q)uestion/,
+            ""
+          );
+          question.question = replacedQuestion;
+          tempFlashCardArray.push(question);
+        } else if (
+          keyName === "answer" ||
+          keyName === "Answer" ||
+          keyName === "answers" ||
+          keyName === "Answers"
+        ) {
+          console.log(ele.answer);
+          let replacedAnswer: string = ele.answer.replace(
+            /^(A|a)nswer:|^(A|a)nswer/,
+            ""
+          );
+          answer.answer = replacedAnswer;
+          tempFlashCardArray.push(answer);
+        }
+      });
+      tempFlashCardsArr.push(tempFlashCardArray);
+    });
+    this.finalFlashcardsArr = tempFlashCardsArr;
+    console.log(
+      "FInal Flash Cards array arr: ----------> ",
+      this.finalFlashcardsArr
+    );
+  }
+
+  keyPointsArraySetter(keyPointsArray: any[]) {
+    let tempKeyPointsArr: any[] = [];
+    keyPointsArray.forEach((keyPoint, keyPointIndex) => {
+      let tempKeyPointArray: any[] = [];
+      keyPoint.forEach((ele: any, eleIndex: any) => {
+        let keyPoint = { keyPoint: "" };
+        let keyName: string = Object.keys(ele)[0];
+        console.log(keyName);
+        if (
+          keyName === "keyPoint" ||
+          keyName === "KeyPoint" ||
+          keyName === "keyPoints" ||
+          keyName === "KeyPoints"
+        ) {
+          console.log(ele.keyPoint);
+          let replacedKeyPoint: string = ele.keyPoint.replace(
+            /^(K|k)ey(P|p)oint:|^(K|k)ey(P|p)oint/,
+            ""
+          );
+          keyPoint.keyPoint = replacedKeyPoint;
+          tempKeyPointArray.push(keyPoint);
+        }
+      });
+      tempKeyPointsArr.push(tempKeyPointArray);
+    });
+    this.finalKeypointsArr = tempKeyPointsArr;
+    console.log(
+      "FInal key points array arr: ----------> ",
+      this.finalKeypointsArr
+    );
   }
 
   // onItemElementsChanged(): void {
